7dea135e5b40b2c6526eabaae2dae79ca31c07f7
Steve Cirelli <scirelli@gmail.com>
2018-04-21 11:30:39 -0500

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # certbot_scripts
 Some certbot scripts for lets encrypt.
+
+Run `getCerts.sh` to update certs.
--- getCerts.sh
+++ getCerts.sh
@@ -5,7 +5,13 @@
                                 -w /var/www/html/scirelli.com/html -d scirelli.com \
                                 -w /var/www/html/athenacirelli.com/html -d athenacirelli.com \
                                 -w /var/www/html/nlsdr.com/html -d nlsdr.com \
-                                -w /var/www/html/jupyter.cirelli.org/html -d jupyter.cirelli.org
+                                -w /var/www/html/jupyter.cirelli.org/html -d jupyter.cirelli.org \
+                                -w /var/www/html/jen.cirelli.org/html -d jen.cirelli.org \
+                                -w /var/www/html/jen.cirelli.org/html -d jenly.cirelli.org \
+                                -w /var/www/html/amiibo.nfc.cirelli.org/html -d amiibo.nfc.cirelli.org \
+                                -w /var/www/html/nfc.cirelli.org/html -d nfc.cirelli.org \
+                                -w /var/www/html/athena.cirelli.org/html -d athena.cirelli.org
+ 
 
 echo "-Congratulations! Your certificate and chain have been saved at /etc/letsencrypt/live/cirelli.org/fullchain.pem. Your cert will expire on 2017-02-25. To obtain a new or tweaked version of this certificate in the future, simply run certbot-auto again. To non-interactively renew *all* of your certificates, run 'certbot-auto renew' - If you like Certbot, please consider supporting our work by: "
 echo "Donating to ISRG / Let's Encrypt:   https://letsencrypt.org/donate"
